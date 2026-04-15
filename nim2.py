dae9f141fb55c32f50b05caf83fa5cb76c9a3c99
happyhandsomeflamingo-svg <happyhandsomeflamingo@gmail.com>
2026-04-14 19:27:13 -0700

    Update nim2.py

--- nim2.py
+++ nim2.py
@@ -1,12 +1,14 @@
 import sys
 
 # Note to debuggers: ^ means XOR function e.g. 3^5=11_2^101_2=110_2=6
+
 try:
     print('enter a set of numbers, seperated by spaces')
     print('choose a heap, take a positive amount from it. last player to move wins')
-except Exception():
+    numbers=sys.stdin.readline()[:-1].split(' ')
+except ValueError:
     print("invalid format, try again")
-numbers=sys.stdin.readline()[:-1].split(' ')
+
 piles=[] #keeping track of all heaps
 for i in enumerate(numbers):
     piles.append(int(i[1]))
@@ -22,40 +24,46 @@ else: #the xor is not zero, the first player wins
     print('You will be second')
 
 def play_game(piles): # Shockingly, it means the Bot's repsonse in which pile and the amount it takes! 🤯
-    # How nim works is that you want to give other other player piles where XOR is 0, then it is a monovariant that eventualy spirals to 0,0,... and they lose. The bot here is lucky and is always given a positive xor, since a zero xor must always change into a positive xor no mattter what the player does
+    # How nim works is that you want to give other player piles where XOR is 0, then it is a monovariant that eventualy spirals to 0,0,... and they lose. The bot here is lucky and is always given a positive xor, since a zero xor must always change into a positive xor no mattter what the player does
     nim_sum=0
     for i in piles:
         nim_sum=nim_sum^i
     bin_sum=str(bin(nim_sum))[2:]
     for heap in range(len(piles)):
         heap_bin=str(bin(piles[heap]))[2:]
-        if heap_bin[-len(bin_sum)]==1:
-            return heap, nim_sum^piles[heap]
+        if len(bin_sum)<=len(heap_bin) and int(heap_bin[-len(bin_sum)])==1:
+            return heap, piles[heap]-(nim_sum^piles[heap])
 if player_turn=='A':
     print('the piles are')
     print(piles)
     print('enter the pile followed by a space then the amount you take')
-    pile,amount=sys.stdin.readline[:-1].split(' ')
-    pile=int(pile) # turning it from integer after split to string
+    pile,amount=sys.stdin.readline()[:-1].split(' ')
+    pile=int(pile)-1 # turning it from integer after split to string
     amount=int(amount)
-    while pile<1 or pile>len(piles) or amount>piles[pile] or amount<1:
+    while pile<0 or pile>=len(piles) or amount>piles[pile] or amount<1:
         print("invalid, try again")
-        pile,amount=sys.stdin.readline[:-1].split(' ')
-        pile=int(pile)
+        pile,amount=sys.stdin.readline()[:-1].split(' ')
+        pile=int(pile)-1
+        amount=int(amount)
     piles[pile]-=amount
-while sum(piles)!=0:
+while True:
+    print('Amount left for bot:')
+    print(piles)
     bot_pile, bot_amount=play_game(piles)
-    print('the bot has taken: '+str(bot_amount)+' from pile '+str(bot_pile))
+    print('the bot has taken: '+str(bot_amount)+' from pile '+str(bot_pile+1))
     piles[bot_pile]-=bot_amount
     print('the piles are')
     print(piles)
+    if sum(piles)==0:
+        break
     print('enter the pile followed by a space then the amount you take')
-    pile,amount=sys.stdin.readline[:-1].split(' ')
-    pile=int(pile) # turning it from integer after split to string
+    pile,amount=sys.stdin.readline()[:-1].split(' ')
+    pile=int(pile)-1 # turning it from integer after split to string
     amount=int(amount)
-    while pile<1 or pile>len(piles) or amount>piles[pile] or amount<1:
+    while pile<0 or pile>=len(piles) or amount>piles[pile] or amount<1:
         print("invalid, try again")
-        pile,amount=sys.stdin.readline[:-1].split(' ')
-        pile=int(pile)
+        pile,amount=sys.stdin.readline()[:-1].split(' ')
+        pile=int(pile)-1
+        amount=int(amount)
     piles[pile]-=amount
 print('You lose!...its rigged anyway')
